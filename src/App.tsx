1e953066737a842d118a4eb6db75511d9836bbab
maruti-panchal <panchal.maruti70@gmail.com>
2026-02-10 17:00:35 +0530

    implemented data fetching feature with loading and error state

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@
 // import TimersContextProvider from './components/store/timers-context.tsx';
 // import Timers from './components/Timers.tsx';
 
+import { ReactNode, useEffect, useState } from "react";
+import { get } from "./utils/http";
+import BlogPosts, { BlogPost } from "./components/BlogPosts";
+import fetchingImg from "./assets/data-fetching.png";
+
 // function App() {
 //   return (
 //     <>
@@ -19,12 +24,56 @@
 
 // export default App;
 
-
+type RawDataBlogPost={
+  id:number;
+  title:string;
+  body:string;
+}
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  const [fetchedPosts, setFetchedPosts] =useState<BlogPost[]>([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [fetchError, setFetchError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true);
+      try {
+         const data = await get('https://jsonplaceholder.typicode.com/posts') as RawDataBlogPost[];
+      const blogPosts = data.map(rawPost=>{
+        return{
+          id:rawPost.id,
+          title:rawPost.title,
+          text:rawPost.body
+        }
+      });
+      setFetchedPosts(blogPosts);
+      } catch (error) {
+        if(error instanceof Error){
+          console.error("Error fetching blog posts:", error.message);
+        }else{
+          console.error("Unknown error fetching blog posts:", error);
+        }
+        setFetchError("Failed to Fetch Blog Posts");
+      }
+        setIsFetching(false);
+    }
+      fetchPosts();
+  }, []);
+
+let content:ReactNode;
+
+if(fetchedPosts){
+  content=<BlogPosts posts={fetchedPosts}/>
+} 
+
+if(isFetching){
+  content=<p id="loading-fallback">Loading...</p>
+}
+  return <main>
+    <img src={fetchingImg} alt="Data Fetching"/>
+    {content}
+  </main>
 }
 
 export default App
