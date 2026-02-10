4ea3039a97048c074f359a8cb393e2cbeb59c063
maruti-panchal <panchal.maruti70@gmail.com>
2026-02-10 16:06:50 +0530

    Timmer project is done.setup project for data - fetching (Blog)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
-import AddTimer from './components/AddTimer.tsx';
-import Header from './components/Header.tsx';
-import TimersContextProvider from './components/store/timers-context.tsx';
-import Timers from './components/Timers.tsx';
+// import AddTimer from './components/AddTimer.tsx';
+// import Header from './components/Header.tsx';
+// import TimersContextProvider from './components/store/timers-context.tsx';
+// import Timers from './components/Timers.tsx';
 
-function App() {
+// function App() {
+//   return (
+//     <>
+//       <TimersContextProvider>
+//         <Header />
+//         <main>
+//           <AddTimer />
+//           <Timers />
+//         </main>
+//       </TimersContextProvider>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+
+const App = () => {
   return (
-    <>
-      <TimersContextProvider>
-        <Header />
-        <main>
-          <AddTimer />
-          <Timers />
-        </main>
-      </TimersContextProvider>
-    </>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
